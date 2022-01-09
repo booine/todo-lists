25bff86554384e4511a4bc6180d552854b531254
Bonnie Lin <qwer821218@gmail.com>
2022-01-09 17:41:04 +0800

    + todos data use v-for to print our todo list

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -3,7 +3,20 @@
 var one = Vue.createApp({
   data: function data() {
     return {
-      addMessage: ''
+      addMessage: '',
+      todos: [{
+        id: 1,
+        content: '選項1',
+        complete: true
+      }, {
+        id: 2,
+        content: '選項2',
+        complete: true
+      }, {
+        id: 3,
+        content: '選項3',
+        complete: false
+      }]
     };
   }
 }).mount('#app');
--- main.js
+++ main.js
@@ -1,7 +1,20 @@
 const one = Vue.createApp({
     data() {
         return {
-            addMessage: ''
+            addMessage: '',
+            todos: [{
+                id: 1,
+                content: '選項1',
+                complete: true
+            }, {
+                id: 2,
+                content: '選項2',
+                complete: true
+            }, {
+                id: 3,
+                content: '選項3',
+                complete: false
+            }, ]
         };
     },
 
