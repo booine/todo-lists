89981475f8e2efa59d082d4dd306ee5dee2422aa
Bonnie Lin <qwer821218@gmail.com>
2022-01-09 20:55:33 +0800

    +create todo

--- dist/main.dev.js
+++ dist/main.dev.js
@@ -7,7 +7,7 @@ var one = Vue.createApp({
       todos: [{
         id: 1,
         content: '選項1',
-        complete: true
+        complete: false
       }, {
         id: 2,
         content: '選項2',
@@ -18,5 +18,20 @@ var one = Vue.createApp({
         complete: false
       }]
     };
+  },
+  methods: {
+    createTodo: function createTodo() {
+      // 因使用者輸入而觸發。
+      var newTodo = {
+        //建立新的 object，將使用者的輸入值寫入todos array中。
+        id: this.todos.lenght + 1,
+        // id這裡通常會跟後端用 API 做連接。這裡先用其他方法代替。
+        content: this.addMessage,
+        complete: false
+      };
+      this.todos.push(newTodo); // 向陣列 todos 後方新增新的 object。
+
+      this.addMessage = ''; //清空輸入格
+    }
   }
 }).mount('#app');
--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const one = Vue.createApp({
             todos: [{
                 id: 1,
                 content: '選項1',
-                complete: true
+                complete: false
             }, {
                 id: 2,
                 content: '選項2',
@@ -17,5 +17,20 @@ const one = Vue.createApp({
             }, ]
         };
     },
+    methods: {
+        createTodo() {
+            // 因使用者輸入而觸發。
+            let newTodo = {
+                //建立新的 object，將使用者的輸入值寫入todos array中。
+                id: this.todos.lenght + 1,
+                // id這裡通常會跟後端用 API 做連接。這裡先用其他方法代替。
+                content: this.addMessage,
+                complete: false
+            };
+            this.todos.push(newTodo); // 向陣列 todos 後方新增新的 object。
+
+            this.addMessage = ''; //清空輸入格
+        }
+    }
 
 }).mount('#app');
