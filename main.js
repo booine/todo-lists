7f4b0cf86a43399628be3e715bb649ec4f5f9cfc
Bonnie Lin <qwer821218@gmail.com>
2022-01-13 21:57:57 +0800

    + auto focus made

--- main.js
+++ main.js
@@ -37,17 +37,24 @@ const one = Vue.createApp({
         deleteTodo(todo, index) {
             // 製作確認是否刪除的對話視窗： confirm(message);
             // confirm(message);會跳出帶有「取消」與「確認」鍵的對話視窗，執行完會回傳布林值，按取消就回傳(false)、按確認就回傳(true)
-            // let result = confirm('Delete todo : ' + todo.content + ' ?'); //舊寫法
+            //舊寫法:let result = confirm('Delete todo : ' + todo.content + ' ?');
             let result = confirm(`Delete todo : ${todo.content} ?`); //新寫法，利用 ${變數或參數或引數}帶入變數、參數或引數內容。
             if (result) {
                 this.todos.splice(index, 1);
             }
         },
-
-        enterEditMode(todo) { // 雙點擊後進入【編輯模式】：
+        // 雙點擊後進入【編輯模式】& Auto focus：
+        enterEditMode(todo, e) {
+            let input = e.target.nextElementSibling;
+            //找到需要 Auto focus 的 DOM 節點，在此例因為 div 和 input 是 sibling 關係，所以使用 js原生屬性「nextElementSibling」去把 <input> 找出並存放於變數中。
             this.editModeId = todo.id; // 要作為 v-show 的判斷依據。
             this.beforeContent = todo.content;
             // 將原本的輸入先暫存在 beforeContent 中，以便取消編輯後重新寫回。
+
+            this.$nextTick(() => {
+                input.focus();
+            });
+            // $nextTick(函式);用以確保前面動作已re-render後，下一輪 re-render 再執行nextTick內的動作。(和vue的lifecycle有關。)
         },
 
         updateTodo() { // (完成編輯)當按下 enter 後執行：
@@ -57,7 +64,7 @@ const one = Vue.createApp({
         cancelUpdateTodo(todo) { // (取消編輯)當按下 esc 後執行並回復到原先的內容：
             this.leaveEditMode();
             todo.content = this.beforeContent;
-            // 將原先的內容重新寫回(所以才要傳入todo參數)。
+            //將原先的內容重新寫回(所以才要傳入todo參數)。
         },
 
         leaveEditMode() { // 離開【編輯模式】：
